f96516e5db620c62a63e60282f133b605b194b12
David_Motari <davemotari22@gmail.com>
2023-02-01 05:50:44 +0300

    Selection of best match language implementation

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+2-app.py
+finds supported languages
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
+
+
+class Config(object):
+    """
+    configuration of available languages in the app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def greetings():
+    """
+    Initialization of app
+    """
+    return render_template('1-index.html', strict_slashes=False)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determine best match from supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
