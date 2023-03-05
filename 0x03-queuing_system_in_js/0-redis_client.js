d38ec82d722f2b65492ee3a852ea133a25671c2d
David Motari <david@Xubuntu.myguest.virtualbox.org>
2023-03-05 04:52:37 +0300

    Creates connection to redis server

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,11 @@
-import { createClient } from 'redis';
+import { createClient } from 'redis'; // importing redis_node module createClient function
 
-const client = createClient();
+const client = createClient(); // Creating client
 
 client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error.message}`);
+  console.log(`Redis client not connected to the server: ${error.message}`); // output incase of errror
 });
 
 client.on('ready', () => {
-    console.log('Redis client connected to the server');
+    console.log('Redis client connected to the server'); // output on connection
 });
